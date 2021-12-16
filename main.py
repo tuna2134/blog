2b711b2c1046ce9a5d8f3a40bd221d34680aa69b
tnt <45692264+tuna2134@users.noreply.github.com>
2021-12-17 08:01:00 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from sanic import Sanic
+from sanic.response import html, file
+from jinja2 import Environment, FileSystemLoader
+
+app = Sanic("main")
+
+env = Environment(loader=FileSystemLoader('./templates/', encoding='utf8'), enable_async=True)
+
+async def template(tpl, **kwargs):
+    template = env.get_template(tpl)
+    content = await template.render_async(kwargs)
+    return html(content)
+  
+@app.route("/<path:path>")
+async def main(request):
+    if path.endswith(".html"):
+        return await template(path)
+    else:
+        return await file(path)
