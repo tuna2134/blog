f3251c080a564711e878f20c56b58abf5b05dc09
tnt <45692264+tuna2134@users.noreply.github.com>
2021-12-17 03:18:48 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,5 +6,6 @@ req = ajax.Ajax()
 req.open("GET", "https://randomuser.me/api/", False)
 req.send()
 data = json.loads(req.text)["results"][0]
+# 反映
 document['main'] <= html.H1(f'{data["name"]["title"]}.{data["name"]["first"]} {data["name"]["last"]}')
 document['main'] <= html.IMG(src=data["picture"]["medium"], alt="image")
